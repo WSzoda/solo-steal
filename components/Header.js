5ed84f8368959c7f9afc8557808f7223a72c9983
Wowosz <wojciechszoda@icloud.com>
2022-12-16 08:22:47 +0100

    added SideMenu

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,38 @@
 import React from 'react'
 import Link from 'next/link'
+import { useState } from 'react'
 
 import { GiHamburgerMenu } from 'react-icons/gi'
 import styles from '../styles/Header.module.scss'
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
   return (
     <div className={styles.navBarContainer}>
-        <Link href="/"><GiHamburgerMenu/></Link>
-        <div className={styles.navBarLinksContaine}>
-            <ul>
-                <li><Link href="/">Profil</Link></li>
-                <li><Link href="/">Kursy</Link></li>
-                <li><Link href="/">Lekcja</Link></li>
-            </ul>
+      <button type='button'
+      onClick={() => {setOpenMenu(!openMenu)}}
+      className={styles.hamburgerButton}
+      ><GiHamburgerMenu/></button>
+      <div className={openMenu ? `${styles.hamburgerSlider} ${styles.opened}` : `${styles.hamburgerSlider}`}>
+        <button type='button'
+         onClick={() => {setOpenMenu(!openMenu)}}
+         className={styles.hamburgerButton}
+         ><GiHamburgerMenu/></button>
+        <div className={styles.navBarContentContainer}>
+          <h3>Główne</h3>
+          <ul className={styles.navLinks}>
+            <li><Link href="/">Profil</Link></li>
+            <li><Link href="/">Kursy</Link></li>
+            <li><Link href="/">Lekcja</Link></li>
+          </ul>
+          <h3>Kursy</h3>
+          <ul className={styles.navLinks}>
+            <li><Link href="/">UEiTP</Link></li>
+            <li><Link href="/">PEiE</Link></li>
+            <li><Link href="/">AK</Link></li>
+          </ul>
         </div>
+      </div>
     </div>
   )
 }
--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 
 import Header from './Header'
 import Footer from './Footer'
@@ -7,14 +8,19 @@ import Footer from './Footer'
 const Layout = ({ children }) => {
   return (
     <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+    </Head>
     <div>
-        <Header/>
+      <Header/>
     </div>
     <div>
-        {children}
+      {children}
     </div>
     <div>
-        <Footer/>
+      <Footer/>
     </div>
     </>
   )
