a17c4ba23391bb0503ebd7fa07a0823a76a4a423
Wojciech Szoda <wojciechszoda@icloud.com>
2022-12-24 22:43:43 +0100

    Basic footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,39 @@
 import React from 'react'
 
+import styles from '../styles/Footer.module.scss'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.footerContainer}>
+      <div className={styles.columns}>
+        <div className={styles.column}>
+          <h3>Główne</h3>
+          <ul>
+            <li>Profil</li>
+            <li>Kursy</li>
+            <li>Lekcja</li>
+          </ul>
+        </div>
+        <div className={styles.column}>
+          <h3>Kursy</h3>
+          <ul>
+            <li>UEiTP</li>
+            <li>PEiE</li>
+            <li>AK</li>
+          </ul>
+        </div>
+        <div className={styles.column}>
+          <h3>Konto</h3>
+          <ul>
+            <li>Zaloguj</li>
+            <li>Ucz się</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        <p>Copright Wojtek</p>
+      </div>
+    </div>
   )
 }
 
